0022bb560323d7dc2d922bde8b9de11eb561662f
iugo <iugogogo@gmail.com>
2025-06-27 23:10:58 +0800

    更新开发任务以允许所有权限，重构动态导入逻辑为异步函数以提高可读性。

--- main.ts
+++ main.ts
@@ -1,7 +1,11 @@
 const filePath = "/dynamic-import/test.ts";
-const fn = await import(import.meta.dirname + filePath).then((m) => m.add);
 
-// Learn more at https://docs.deno.com/runtime/manual/examples/module_metadata#concepts
+async function getFn(filePath: string): Promise<((a: number, b: number) => number)> {
+  const code = await import(import.meta.dirname + filePath)
+  return code.add;
+}
+
 if (import.meta.main) {
+  const fn = await getFn(filePath);
   console.log("Add 2 + 3 =", fn(2, 3));
 }
