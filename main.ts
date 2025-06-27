adcfee2f538ba05c87058de587276ca9e24541bf
iugo <iugogogo@gmail.com>
2025-06-27 23:23:42 +0800

    更新构建任务以包含页面目录，重构主程序以支持动态路由，添加 HTTP 服务启动和测试步骤，删除不再需要的动态导入示例文件。

--- main.ts
+++ main.ts
@@ -1,11 +1,25 @@
-const filePath = "/dynamic-import/test.ts";
+Deno.serve({ port: 9000 }, async (req) => {
+  const url = new URL(req.url);
 
-async function getFn(filePath: string): Promise<((a: number, b: number) => number)> {
-  const code = await import(import.meta.dirname + filePath)
-  return code.add;
-}
+  const routerFilePath = import.meta.dirname +
+    `/pages${url.pathname}.ts`;
 
-if (import.meta.main) {
-  const fn = await getFn(filePath);
-  console.log("Add 2 + 3 =", fn(2, 3));
-}
+  console.log(`routerFilePath: ${routerFilePath}`);
+  const fn = await import(routerFilePath)
+    .then((m) => {
+      const fn = m[req.method.toLowerCase()];
+      if (typeof fn === "function") {
+        return fn;
+      }
+      console.error("路由处理文件中没有路由处理函数, 程序可能有误");
+      return null;
+    }).catch((err) => {
+      console.error("没有找到路由处理文件, 可能为非法访问:", err);
+      return null;
+    });
+
+  if (!fn) {
+    return new Response("Not Found", { status: 404 });
+  }
+  return fn(req);
+});
